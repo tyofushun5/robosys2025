260f9dcf38c3a5b7131e2b2a468d624373eb34d1
tyofushun5 <zenshun10@gmail.com>
2025-10-30 12:57:58 +0900

    fix test

--- test.bash
+++ test.bash
@@ -2,6 +2,7 @@
 # SPDX-FileCopyrightText: 2025 Shunsuke Fukuoks
 # SPDX-License-Identifier: BSD-3-Clause
 
+
 ng () {
 	echo ${1}行目が違うよ
 	res=1
@@ -11,6 +12,14 @@ res=0
 
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
