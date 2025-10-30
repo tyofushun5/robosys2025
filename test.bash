f1ce3a88b1eacfd0d573b7dea69af8d391f23ce6
tyofushun5 <zenshun10@gmail.com>
2025-10-30 12:36:19 +0900

    add testcode

---
+++ test.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2025 Shunsuke Fukuoks
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo ${1}行目が違うよ
+	res=1
+}
+
+res=0 
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
+exit $res
