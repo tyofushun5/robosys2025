0fd3fdb3b4202365ff00a52137012813745982e8
tyofushun5 <zenshun10@gmail.com>
2025-11-28 04:13:08 +0900

    11/28 Fix tools.py

--- tools/tools.py
+++ tools/tools.py
@@ -19,3 +19,22 @@ def to_float_list(func):
                 float_args.append(converted)
         return func(*float_args, **kwargs)
     return wrapper
+
+
+def output(func):
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        result = func(*args, **kwargs)
+        try:
+            iterator = iter(result)
+        except TypeError:
+            print(result)
+            return result
+
+        if isinstance(result, (str, bytes)):
+            print(result)
+            return result
+
+        for item in iterator:
+            print(item)
+        return result
+    return wrapper
